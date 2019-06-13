c8eb51d9c0c8c8cd9eb62ec0a0edf363ad8fefeb
pivey <peteriveyhansen@gmail.com>
2019-06-13 21:49:52 +0200

    added styling

--- spa-survivors-clock/src/App.js
+++ spa-survivors-clock/src/App.js
@@ -9,7 +9,10 @@ function App() {
       <Board citiesObj = {[ 
       {city:'Stockholm', region:'Europe'},
       {city:'London', region:'Europe'},
-      {city:'Tehran', region:'Asia'}]}/>
+      {city:'Tehran', region:'Asia'},
+      {city:'Chicago', region:'America'},
+      {city:'Omsk', region:'Asia'},
+      {city:'Adelaide', region:'Australia'}]}/>
     </div>
   );
 }
@@ -20,6 +23,9 @@ function Board(props) {
       <Clock cityObj={props.citiesObj[0]}/>
       <Clock cityObj={props.citiesObj[1]}/>
       <Clock cityObj={props.citiesObj[2]}/>
+      <Clock cityObj={props.citiesObj[3]}/>
+      <Clock cityObj={props.citiesObj[4]}/>
+      <Clock cityObj={props.citiesObj[5]}/>
 
     </div>
   )
@@ -33,6 +39,7 @@ let [count, setCount] = useState(0);
 useEffect(() => {
    function timer() {
      setTimeout (() => {
+       // eslint-disable-next-line
       setCount(count+=1);
       timer();
     },1000); 
@@ -41,12 +48,14 @@ useEffect(() => {
 }, []);
 const cityRegion = props.cityObj.region + '/' + props.cityObj.city; 
 
+// document.body.style.background = "url('[INSERT HTTPS IMAGE URL HERE]')";
+
   return (
-    <div className="Clock">
-        <h1>
+    <div className="clock-flex" style={{backgroundImage: `url('https://source.unsplash.com/random/500x500?${props.cityObj.city}'`}}>
+        <h1 className ="city">
           {props.cityObj.city}
         </h1>
-        <h3>
+        <h3 className="time">
           {moment().tz(cityRegion).format('LTS')}
           {/* {new Date().toLocaleTimeString()} */}
         </h3>

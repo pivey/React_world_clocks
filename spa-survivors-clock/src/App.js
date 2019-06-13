2429e7ae18c1fd1515094892e84533de1ff5ba4e
pivey <peteriveyhansen@gmail.com>
2019-06-13 16:58:32 +0200

    adding a react hook to the clock

---
+++ spa-survivors-clock/src/App.js
@@ -0,0 +1,57 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Board citiesObj = {[ 
+      {city:'Stockholm',timeZone:1},
+      {city:'London',timeZone:2},
+      {city:'Tehran',timeZone:3}]}/>
+    </div>
+  );
+}
+
+function Board(props) {
+  return (
+    <div className="Board">
+      <Clock cityObj={props.citiesObj[0]}/>
+      <Clock cityObj={props.citiesObj[1]}/>
+      <Clock cityObj={props.citiesObj[2]}/>
+
+    </div>
+  )
+}
+
+
+function Clock(props) {
+const [count, setCount] = useState(0); 
+
+useEffect(() => {
+   function timer () {
+     setTimeout (() => {
+      setCount(count+1);
+    },1000); }
+    timer();
+}, []);
+
+  return (
+    <div className="Clock">
+        <h1>
+          {props.cityObj.city}
+        </h1>
+        <h3>
+          {props.cityObj.timeZone }
+          <br/>
+          {new Date().toLocaleTimeString()}
+          <br/>
+          {count}
+        </h3>
+    </div>
+  );
+}
+
+
+export default App;
+
+

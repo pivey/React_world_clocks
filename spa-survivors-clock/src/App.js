44a0ca49a6e9f25a816c7cd0d244b576ecc6d794
SirPoyan <poyan.delavari@gmail.com>
2019-06-14 10:32:06 +0200

    t

--- spa-survivors-clock/src/App.js
+++ spa-survivors-clock/src/App.js
@@ -1,69 +1,83 @@
 import React, {useEffect, useState} from 'react';
-import moment from 'moment'; 
+import moment from 'moment';
 import "moment-timezone";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Board citiesObj = {[ 
-      {city:'Stockholm', region:'Europe'},
-      {city:'London', region:'Europe'},
-      {city:'Tehran', region:'Asia'},
-      {city:'Chicago', region:'America'},
-      {city:'Omsk', region:'Asia'},
-      {city:'Adelaide', region:'Australia'}]}/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Board
+                citiesObj={[
+                {
+                    city: 'Stockholm',
+                    region: 'Europe'
+                }, {
+                    city: 'London',
+                    region: 'Europe'
+                }, {
+                    city: 'Tehran',
+                    region: 'Asia'
+                }, {
+                    city: 'Chicago',
+                    region: 'America'
+                }, {
+                    city: 'Omsk',
+                    region: 'Asia'
+                }, {
+                    city: 'Adelaide',
+                    region: 'Australia'
+                }
+            ]}/>
+        </div>
+    );
 }
 
 function Board(props) {
-  return (
-    <div className="Board">
-      <Clock cityObj={props.citiesObj[0]}/>
-      <Clock cityObj={props.citiesObj[1]}/>
-      <Clock cityObj={props.citiesObj[2]}/>
-      <Clock cityObj={props.citiesObj[3]}/>
-      <Clock cityObj={props.citiesObj[4]}/>
-      <Clock cityObj={props.citiesObj[5]}/>
+    return (
+        <div className="Board">
+            <Clock cityObj={props.citiesObj[0]}/>
+            <Clock cityObj={props.citiesObj[1]}/>
+            <Clock cityObj={props.citiesObj[2]}/>
+            <Clock cityObj={props.citiesObj[3]}/>
+            <Clock cityObj={props.citiesObj[4]}/>
+            <Clock cityObj={props.citiesObj[5]}/>
 
-    </div>
-  )
+        </div>
+    )
 }
-
 
 function Clock(props) {
- 
-let [count, setCount] = useState(0); 
+    let [count,
+        setCount] = useState(0)
+    function timer() {
+        setTimeout(() => {
+            setCount(count + 1);
+            timer();
+        }, 500);
+    }
+    timer();
 
-useEffect(() => {
-   function timer() {
-     setTimeout (() => {
-       // eslint-disable-next-line
-      setCount(count+=1);
-      timer();
-    },1000); 
-  }
-  timer();
-}, []);
-const cityRegion = props.cityObj.region + '/' + props.cityObj.city; 
+    const cityRegion = props.cityObj.region + '/' + props.cityObj.city;
 
-// document.body.style.background = "url('[INSERT HTTPS IMAGE URL HERE]')";
+    // document.body.style.background = "url('[INSERT HTTPS IMAGE URL HERE]')";
 
-  return (
-    <div className="clock-flex" style={{backgroundImage: `url('https://source.unsplash.com/random/500x500?${props.cityObj.city}'`}}>
-        <h1 className ="city">
-          {props.cityObj.city}
-        </h1>
-        <h3 className="time">
-          {moment().tz(cityRegion).format('LTS')}
-          {/* {new Date().toLocaleTimeString()} */}
-        </h3>
-    </div>
-  );
+    return (
+        <div
+            className="clock-flex"
+            style={{
+            backgroundImage: `url('https://source.unsplash.com/random/500x500?${props.cityObj.city}'`
+        }}>
+            <h1 className="city">
+                {props.cityObj.city}
+            </h1>
+            <h3 className="time">
+                {moment()
+                    .tz(cityRegion)
+                    .format('LTS')}
+                {/* {new Date().toLocaleTimeString()} */}
+            </h3>
+        </div>
+    );
 }
 
-
 export default App;
-
-
